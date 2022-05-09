e9ff187363dd2e4b49fa5c721c523a98d72078a2
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-09 17:32:22 -0400

    troubleshooting service worker

--- service-worker.js
+++ service-worker.js
@@ -9,22 +9,20 @@ const FILES_TO_CACHE = [
     "./public/js/index.js"
 ];
 
-// self.addEventListener('fetch', function (e) {
-//     console.log('fetch request : ' + e.request.url)
-//     e.respondWith(
-//         caches.match(e.request).then(function (request) { // if cache is available, respond w/ cache
-//             if (request) {
-//                 console.log('responding with cache : ' + e.request.url)
-//                 return request
-//             } else { // if nothing in the cache, normal fetch request
-//                 console.log('file is not cached, fetching : ' + e.request.url)
-//                 return fetch(e.request)
-//             }
-
-//         })
-
-//     )
-// });
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) { // if cache is available, respond w/ cache
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { // if nothing in the cache, normal fetch request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
 
 self.addEventListener('install', function (e) {
 
