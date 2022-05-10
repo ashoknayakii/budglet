ba424354f4e2a1cfb84c278fac0aeb1bc0bc1250
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-10 18:49:51 -0400

    moved service worker file to public folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,20 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
-    "./public/icons",
-    "./public/css/styles.css",
-    "./public/js/index.js"
+    "./css/styles.css",
+    "./js/idb.js",
+    "./js/index.js",
+    // "./manifest.webmanifest",
+    "./public/icons/icon-72x72.png",
+    "./public./icons/icon-96x96.png",
+    "./public/icons/icon-128x128.png",
+    "./public/icons/icon-144x144.png",
+    "./public/icons/icon-152x152.png",
+    "./public/icons/icon-192x192.png",
+    "./public/icons/icon-384x384.png",
+    "./public/icons/icon-512x512.png"
 ];
 
 self.addEventListener('fetch', function (e) {
