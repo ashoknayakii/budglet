8ef39e1141906b5267878a61495e11148a4c7895
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-21 19:27:55 -0400

    upload transaction function working properly cache is cleared when online

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,82 +1,82 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'budglet' and set it to version 1
-const request = indexedDB.open('budglet', 1);
+const request = indexedDB.open("budglet", 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+  // save a reference to the database
+  const db = event.target.result;
+  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
-// upon a successful 
+// upon a successful
 request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
 
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
-    if (navigator.onLine) {
-        // we haven't created this yet, but we will soon, so let's comment it out for now
-        uploadTransaction();
-    }
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    uploadTransaction();
+  }
 };
 
 request.onerror = function (event) {
-    // log error here
-    console.log(event.target.errorCode);
+  // log error here
+  console.log(event.target.errorCode);
 };
 
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-    // access the object store for `new_transaction`
-    const budgletObjectStore = transaction.objectStore('new_transaction');
+  // access the object store for `new_transaction`
+  const budgletObjectStore = transaction.objectStore("new_transaction");
 
-    // add record to your store with add method
-    budgletObjectStore.add(record);
+  // add record to your store with add method
+  budgletObjectStore.add(record);
 }
 
 function uploadTransaction() {
+  console.log("Going online");
 
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-    const budgletObjectStore = transaction.objectStore('new_transaction');
+  const budgletObjectStore = transaction.objectStore("new_transaction");
 
-    const getAll = budgletObjectStore.getAll();
+  const getAll = budgletObjectStore.getAll();
 
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_transaction"], "readwrite");
 
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+          const budgletObjectStore = transaction.objectStore("new_transaction");
 
-                    const budgletObjectStore = transaction.objectStore('new_transaction');
+          budgletObjectStore.clear();
 
-                    budgletObjectStore.clear();
-
-                    alert('All saved transactions have been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            });
-        }
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 }
 
-window.addEventListener('online', uploadTransaction);
+window.addEventListener("online", uploadTransaction);
